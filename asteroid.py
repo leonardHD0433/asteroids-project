f78e1b43874b10d98efcc511aa52df251d35f4cc
edwin <edwinchuajinrui@gmail.com>
2025-02-01 02:10:20 +0800

    added new asteroid field

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        print(f"Creating asteroid at {x}, {y} with radius {radius}")
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen,(0,255,255), (self.position.x, self.position.y), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
